b861f9d8447f9d232633b3c952ef9409aa72da75
fcamaria <fernandacoloma7@gmail.com>
2026-03-10 23:11:25 -0700

    chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -25,16 +25,14 @@ if "messages" not in st.session_state:
   for message in st.session_state.messages:
     with st.chat_message(message["role"]):
       st.markdown(message["content"])
-
+      
 # Accept user input
 if prompt := st.chat_input("What is up?"):
-
     # Display user message in chat message container
-  with st.chat_message("user"):
-    st.markdown(prompt)
-  
-  # Add user message to chat history
-  st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+      st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
 
   # Display assistant response in chat message container
   with st.chat_message("assistant"):
